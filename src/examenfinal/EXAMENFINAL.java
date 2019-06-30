5fad7b857b8fd759e53e0a7f2c21b8acae627a0f
Stiven Melendez <stivenrafael@hotmail.com>
2019-06-30 18:28:18 -0500

    se comenzo el examen se agregaron los arraylist

---
+++ src/examenfinal/EXAMENFINAL.java
@@ -0,0 +1,19 @@
+package examenfinal;
+import java.util.*;
+public class EXAMENFINAL {
+    public static ArrayList <String> nombre = new ArrayList(); 
+    public static ArrayList <Double> velocidad = new ArrayList();
+    public static ArrayList <Double> tiempo = new ArrayList();
+    public static Scanner leer = new Scanner(System.in);
+    public static void main(String[] args) 
+    {
+        int cantE;
+        System.out.println("ingrese la cantidad de estudiantes");
+        cantE = leer.nextInt();
+        for(int x=0; x<cantE; x++)
+        {
+            
+        }
+    }
+    
+}
