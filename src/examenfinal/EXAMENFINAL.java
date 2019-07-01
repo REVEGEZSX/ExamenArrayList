322f1f12851aeb728796db2258121a3e4da9777a
Stiven Melendez <stivenrafael@hotmail.com>
2019-06-30 19:50:11 -0500

    terminado

--- src/examenfinal/EXAMENFINAL.java
+++ src/examenfinal/EXAMENFINAL.java
@@ -7,7 +7,8 @@ public class EXAMENFINAL {
     public static Scanner leer = new Scanner(System.in);
     public static void main(String[] args) 
     {
-        int t=1, x=0;
+        int t=1, x=0, posf = 0;
+        float promedioaux=0, fast = 0, promediot=0, essp=0;
         while(t!=0)
         {
             System.out.println("ingrese el nombre del estudiante "+(x+1));
@@ -15,16 +16,30 @@ public class EXAMENFINAL {
             System.out.println("ingrese la velocidad del estudiante "+(x+1));
             velocidad.add(leer.nextFloat());
             tiempo.add(100/velocidad.get(x));
-            System.out.println("Desea terminar?/ ingrese 0 para salir");
+            promedioaux = promedioaux + tiempo.get(x);
+            if(velocidad.get(x) > fast)
+            {
+                fast = velocidad.get(x);
+                posf = x;
+            }
+            System.out.println("ingrese 0 para salir o 1 para continuar");
             t = leer.nextInt();
             x=x+1;
         }
+        promediot = promedioaux/tiempo.size();
         System.out.println("Lista de estudiantes");
         System.out.println("Nombre___Velocidad_____tiempo");
         for(int i=0; i<nombre.size(); i++)
         {
             System.out.println(nombre.get(i)+"_______"+velocidad.get(i)+"________"+tiempo.get(i));
-        }
+            if(tiempo.get(i) > promediot)
+            {
+                essp++;
+            }
+        }        
+        System.out.println("el estudiante mas veloz fue "+nombre.get(posf));
+        System.out.println("el promedio de tiempo fue "+promediot);
+        System.out.println("la cantidad estudiantes sobre del promedio son "+essp);
     }
     
 }
