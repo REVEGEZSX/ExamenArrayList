8eff4e27f68a0ca8a1937e4987962c981ce36914
Stiven Melendez <stivenrafael@hotmail.com>
2019-06-30 19:07:03 -0500

    se termino la prueba con ciclo determinado

--- src/examenfinal/EXAMENFINAL.java
+++ src/examenfinal/EXAMENFINAL.java
@@ -2,8 +2,8 @@ package examenfinal;
 import java.util.*;
 public class EXAMENFINAL {
     public static ArrayList <String> nombre = new ArrayList(); 
-    public static ArrayList <Double> velocidad = new ArrayList();
-    public static ArrayList <Double> tiempo = new ArrayList();
+    public static ArrayList <Float> velocidad = new ArrayList();
+    public static ArrayList <Float> tiempo = new ArrayList();
     public static Scanner leer = new Scanner(System.in);
     public static void main(String[] args) 
     {
@@ -15,9 +15,13 @@ public class EXAMENFINAL {
             System.out.println("ingrese el nombre del estudiante "+(x+1));
             nombre.add(leer.next());
             System.out.println("ingrese la velocidad del estudiante "+(x+1));
-            velocidad.add(leer.nextDouble());
-            
+            velocidad.add(leer.nextFloat());
             tiempo.add(100/velocidad.get(x));
+        }
+        System.out.println("Lista de estudiantes");
+        for(int x=0; x<cantE; x++)
+        {
+            System.out.println(nombre.get(x)+" "+velocidad.get(x)+" "+tiempo.get(x));
         }
     }
     
